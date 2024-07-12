e58d1da7150da21c0e19a8b9fa271885c6872686
pavanudavant825 <pavanudavant66@gmail.com>
2024-07-12 08:32:57 +0530

    first commit

---
+++ addRecipe.js
@@ -0,0 +1,76 @@
+export const recipe=[
+    {
+        title:'Pavbhaji',
+        ingredients:'oil, garlic, onions, potato, tomatoes, masala, butter, salt,water.',
+        instructions:'Heat the oil in a wok over medium heat. Saute garlic and green chile for 30 seconds, then stir in onions and ginger. Cook until onions are brown. Add tomatoes, and cook until pasty. Stir in cauliflower, cabbage, peas, carrots and potatoes. Season with pav bhaji masala. Cover, and cook for 15 minutes, stirring occasionally. Season with salt, and stir in lemon juice.',
+        description:'This is a very popular recipe in India, liked by almost everyone. You may have to go to a nearby Asian Indian store to get the Pav Bhaji Masala that is the special spice to bring the flavor (Masala means spice). Pav is actually the buns. Bhaji is the vegetables cooked with spice.',
+        image:'',
+    },
+
+    {
+        title:'Paneer Masala',
+        ingredients:'paneer, butter, onion, tomato, cashew, butter, masala, ginger,water.',
+        instructions:'Melt butter in a skillet over medium heat. Add paneer; cook and stir until golden, about 5 minutes. Add onions, bell pepper, jalapeños, ground cashews, garlic paste, ginger paste, cayenne pepper, cumin, coriander, and garam masala; cook and stir until well combined and fragrant, about 1 minute.Mix tomato sauce, half-and-half, and salt into paneer mixture; simmer until thickened, about 30 minutes.',
+        description:'Paneer Butter Masala is one of India’s most popular paneer recipes, and with good reason! Indian cottage cheese cubes are smothered in a creamy, lightly spiced tomato sauce that is downright delicious.',
+        image:'',
+    },
+
+    {
+        title:'Gulab Jamun',
+        ingredients:'water, sugar, baking soda, cashew, ground cordamom, rose water.',
+        instructions:'Combine water, sugar, cardamom, rose water, and saffron in a saucepan; bring to a boil. Reduce heat and simmer until consistency is syrupy, 5 to 10 minutes.Mix milk powder, flour, and baking soda in a bowl until well-combined. Slowly mix in butter and yogurt until dough comes together. Let gulab dough rest for 5 minutes. Shape into smooth balls the size of pennies.Heat oil in a deep-fryer or large saucepan to 350 degrees F (175 degrees C). Test the temperature of the oil by dropping in a small piece of dough. When it sits at the bottom of the pan for 1 minute before coming to the surface, the oil is ready.',
+        description:'These are like little waffle balls that sit in and absorb a sugary rosewater syrup. It was first made on the Indian subcontinent, around South Asia. Gulab Juman originated from Luqmat Al-Qadi, an Arabic dessert. This dessert is popular at weddings, Diwali, Eid Al-Fitr and Eid Al-Adha. There a several different types of jamun with different looks and taste. Serve hot or cold.',
+        image:'',
+    },
+
+    {
+        title:'Pizza',
+        ingredients:'yeast, sugar, bread flour, olive oil, salt, onions, tomoatoes, masala',
+        instructions:'Let the Dough Come to Room Temperature After youre finished mixing the ingredients, allow your dough to rest on the counter for about 5 minutes. This will allow the gluten to relax, making the dough much easier to stretch and shape.',
+        description:'This is a great recipe when you do not want to wait for the dough to rise. You just mix it and allow it to rest for 5 minutes and then it is ready to go! It yields a soft, chewy crust.',
+        image:'',
+    },
+
+    {
+        title:'Pohe',
+        ingredients:'onion, tomato, poha, chilli, oil ',
+        instructions:'Take poha in a large strainer. Wash it 1-2 times by gently swifting them with your fingers under running water. Drain the excess water through a strainer. Sprinkle salt and sugar over it, mix/toss well and keep aside. Heat 2-tablespoons oil in a pan. Add mustard seeds; when they start to splutter, add cumin seeds, green chilies, curry leaves, peanuts and a pinch of asafoetida. Allow them to sizzle until chilies turn crisp for approx. 30-40 seconds.Add chopped onions and sauté until onions turn translucent.',
+        description:'Kande Pohe is a very popular Maharashtrian breakfast, which can also be had as an evening snack. It is healthy and takes only 10-15 minutes to prepare this dish.',
+        image:'',
+    },
+    {
+        title:'Dosa',
+        ingredients:'wheat flour, oil, baking soda, water',
+        instructions:'Combine flour, 2 tablespoons oil, salt, and baking soda together in a bowl. Mix for 1 to 2 minutes. Add 1 3/4 cup water and mix until its lump free, the chunks of flour have dissolved, and the batter has a good consistency, adding more water if needed. Heat 1 tablespoon oil on a griddle over medium heat. Pour some of the batter onto the griddle; spread the batter in a circular motion, slowly yet steadily. Cook until the edges start to brown, 2 to 3 minutes. Remove from the grill. Repeat with remaining batter, adding more oil between each batch.',
+        description:'A dosa is a thin, savoury crepe in South Indian cuisine made from a fermented batter of ground white gram and rice. Dosas are served hot, often with chutney and sambar (a lentil-based vegetable stew). Dosas are popular in South Asia as well as around the world.',
+        image:'',
+    },
+    {
+        title:'Samosa',
+        ingredients:'potato, onions, chilli, turmeric, coriander, ginger, peas , butter, oil, salt',
+        instructions:'Cut potatoes into 1/4-inch cubes. Transfer into a pot of cold water. Stir in salt and bring to a boil over high heat. Simmer over medium heat until cubes are tender but still hold their shape, about 15 minutes. Drain potatoes in a colander and set aside.Heat olive oil and butter in a skillet over medium heat. Add onion. Cook and stir until softened and translucent, about 5 minutes. Meanwhile, combine 1 teaspoon kosher salt, garam masala, cumin, coriander, turmeric, and cayenne in a small bowl for the spice mix.Add serrano pepper, garlic, ginger, and the spice mix to the onions. Cook and stir until mixture is well combined, about 2 minutes. Add peas; cook until heated through and slightly tender, about 3 minutes.',
+        description:'A samosa is a fried South Asian pastry with a savoury filling, mostly vegetables, spiced potatoes, onions, peas, also non-vegetarian meat, or fish.',
+        image:'',
+    },
+    {
+        title:'Noodles',
+        ingredients:'noodles, salt, oil, chilli, masalsa, onions, carrots, tomato, garlic',
+        instructions:'Heat 1 tablespoon vegetable oil in a small skillet. Cook and stir beaten eggs in hot oil until scrambled and firm. Set aside.Heat 1 teaspoon vegetable oil in a large skillet over medium heat. Cook and stir green onions in hot oil until softened, 2 to 3 minutes. Remove green onions to a plate; set aside.Heat remaining 1 teaspoon vegetable oil in the same skillet over medium heat. Cook and stir carrots, peas, and bell pepper separately until softened, removing each to the plate with green onions when done.',
+        description:'Noodle, a cooked egg-and-flour paste prominent in European and Asian cuisine, generally distinguished from pasta by its elongated ribbonlike form. Noodles are commonly used to add body and flavour to broth soups. They are commonly boiled or sautéed and served with sauces and meats or baked in casseroles.',
+        image:'',
+    },
+    {
+        title:'Pasta',
+        ingredients:'pasta, onions, garlic, oil, chilli, mozarella',
+        instructions:'Bring a large pot of lightly salted water to a boil. Cook rotini pasta at a boil until tender yet firm to the bite, about 8 minutes. Drain and rinse with cold water until cool.pasta drained and rinsed. Combine pasta, salami, bell peppers, onion, mozzarella balls, and olives in a large bowl. Add salad dressing and toss to coat. All ingredients added to a large bowl, tossing to coat Season pasta salad with dry salad dressing mix and stir to combine.dry salad mix added to pasta salad Sprinkle with Parmesan cheese before serving. Enjoy!',
+        description:'This Italian pasta salad that is loaded with tri-color rotini pasta, salami, bell peppers, and mozzarella is quick to make and full of flavor for a delicious summer meal.',
+        image:'',
+    },
+    {
+        title:'Pulav',
+        ingredients:'rice, water, carrot, potato, tomato, beans, capsicum, onion, ginger, garlic, oil, salt, chilli, masala, cinnamon, cardamom, coriander leaves',
+        instructions:'Cook rice and set aside. Slice the onions and shred the vegetables fine.Heat ghee or oil in a pan and add the onions and cumin seeds. When they are slightly browned, add the cinnamon, cloves, and crushed cardamom. Stir and fry for another few seconds until fragrant and the onions turn a darker brown. Vegetable Pulao Recipe | Easy Veg Pulav RecipePin. Add the minced garlic and fry for a few more seconds. Throw in the shredded vegetables, chillies and salt. Mix well, lower flame, and cook covered for about 5 mins until the vegetables are soft. Gently mix in the cooked rice. Garnish with coriander leaves and keep partially covered for the flavours to mingle until ready to serve. Serve veg pulao with any Paneer Recipe like Paneer Butter Masala, Palak Paneer, or Kadai Paneer. You can also serve it with a simple Dal Tadka or Soya Chunks Curry.',
+        description:'Pulao is a one pot rice dish made by cooking fragrant basmati rice with aromatic whole spices, herbs & sometimes stock. There are many kinds of pulao made in India like vegetable pulao, Matar pulao, Tawa pulao, Coconut milk pulav, Zarda pulao, Kashmiri pulao, Yakhni Pulao.',
+        image:'',
+    }
+]
---
+++ rf.js
@@ -0,0 +1,8 @@
+import { recipe } from "./addRecipe";
+
+
+for(let i=0;i<recipe.length;i++){
+    const html=`
+    
+    `
+}
